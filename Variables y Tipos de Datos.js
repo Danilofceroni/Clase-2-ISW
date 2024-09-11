78a00e00fb118c167e66a9cfc51d49fc4b74858c
Danilo Fabiani Ceroni Rodriguez <danilo.ceroni1901@alumnos.ubiobio.cl>
2024-09-11 15:15:14 -0300

    ADD ejercicio
    variables y tipos de datos

---
+++ Variables y Tipos de Datos.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo");
+
+let nombre = "Danilo";
+console.log(nombre)
+console.log(typeof nombre);
+
+let edad=23;
+console.log(edad)
+console.log(typeof edad);
+
+let puedoConducir= true;
+console.log(puedoConducir)
+console.log(typeof puedoConducir); // 'Boolean'
